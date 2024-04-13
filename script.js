772aaab22a3721a6ef556399bd66ce54542ccac0
yzde <74788670+yazidears@users.noreply.github.com>
2024-04-13 22:38:36 +0000

    Lights On - FKA Twigs

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 // script.js
 window.addEventListener('scroll', function() {
     const video = document.getElementById('background-video');
-    const content = document.getElementById('content');
     let scrollPosition = window.scrollY;
-    let blurValue = (scrollPosition > 100) ? (scrollPosition - 100) * 0.1 : 0; // Adjust blur calculation if needed
+    let blurValue = (scrollPosition > 100) ? (scrollPosition - 100) * 0.1 : 0;
     video.style.filter = `blur(${Math.min(blurValue, 10)}px)`;
-
-    content.style.opacity = (scrollPosition > 100) ? '1' : '0'; // Fade in content after 100px of scrolling
 });
